902270af933a37c587867bf0008f32a02f520470
Claudia de Jesus Dantas <101577825+claudiadejesusdantas@users.noreply.github.com>
2022-06-22 18:09:59 -0300

    criando class livro

--- aula-java/aula_intellij/primeiro_programa_intellij/src/br/com/dio/PrimeiroPrograma.java
+++ aula-java/aula_intellij/primeiro_programa_intellij/src/br/com/dio/PrimeiroPrograma.java
@@ -8,8 +8,44 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
         System.out.println(gato);
 
+        Livro livro = new Livro("O problema dos três corpos", 300);
+        System.out.println(livro);
    /*   int a = 5;
         int b = 3;
         System.out.println("Hello World!"+(a+b));*/
+    }
+}
+
+class Livro {
+    private String nome;
+    private  Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
