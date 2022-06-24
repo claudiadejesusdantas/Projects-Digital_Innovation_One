0f2c8bcb68b79b17bb800b380d7a0d0143f29271
Claudia de Jesus Dantas <101577825+claudiadejesusdantas@users.noreply.github.com>
2022-06-24 14:25:46 -0300

    aula sobre boas práticas de variáveis professor Thiago Leite

---
+++ aula-java/aula_intellij/aula_variaveis/src/ExercicioUm_ProfessorThiagoLeite/Variaveis.java
@@ -0,0 +1,63 @@
+package ExercicioUm_ProfessorThiagoLeite;
+
+public class Variaveis {
+    public static void main(String[] args) {
+        int i;
+        //int i;
+        int I;
+        //int 1a;
+        // não é uma boa prática ->
+        int _1a;
+        // não é uma boa prática ->
+        int $1a;
+
+        //variáveis dentro de método, devem ser inicializada,
+        //deve ser provido seu valor inicial
+        i = 5;
+        I = 10;
+        _1a = 20;
+        $1a = 7;
+
+        final int j = 10;
+        //final = constante
+        //não pode mudar mais, pois é uma constante
+        // j = 15;
+        //essa variável é permitida com letras e números ->
+        int asrn24688nd;
+        //essa variável não é permitida, pois contém espaço ->
+        //int asrn24 688nd;
+         //não é uma boa prática o underline, apenas nas constantes.
+        int asrn24688_nd = 1;
+        //essa variável não é permitida, pois contém caracter especial ->
+        //int asrn24688%nd = 10;
+
+        asrn24688nd = 100;
+        asrn24688_nd = 10;
+
+        //variável com Cammel Case, aceitável ->
+        int quantidadeProduto = 50;
+        //não segue a boa prática ->
+        //int QuantidadeProduto = 50;
+        //variável constante com underline aceitável ->
+        final int NUMERO_TENTATIVAS = 5;
+        //variável constante com underline não aceitável ->
+        //final int numero_TENTATIVAS = 5;
+        //essa variável não é permitida, pois seu nome está definido como final ->
+        //int QUANTIDADE_OPCOES = 25;
+        // segue as boas práticas, porém a variável não tem especificidade ->
+        //int tpdProd;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($1a);
+
+        System.out.println(j);
+        System.out.println(asrn24688nd);
+        System.out.println(asrn24688_nd);
+
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+
+    }
+}
