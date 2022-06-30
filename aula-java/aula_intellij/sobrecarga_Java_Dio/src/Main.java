90cb6e412ee9f7c4b0810e3ef0e950402385c22e
Claudia de Jesus Dantas <101577825+claudiadejesusdantas@users.noreply.github.com>
2022-06-30 19:56:13 -0300

    aula sobre sobrecarga - professor thiago leite

---
+++ aula-java/aula_intellij/sobrecarga_Java_Dio/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Exercício Quadrilátero");
+        ExercicioUm.area(3);
+        ExercicioUm.area(5,5);
+        ExercicioUm.area(7,8,9);
+        ExercicioUm.area(5f,5f);
+    }
+}
