54ca282586ba74534edaee7a14045f2616eaa591
Claudia de Jesus Dantas <101577825+claudiadejesusdantas@users.noreply.github.com>
2022-07-22 20:51:28 -0300

    exercício temperatura collections

--- aula-java/aula_intellij/collectionsJava/lists/src/AtividadeTemperatura.java
+++ aula-java/aula_intellij/collectionsJava/lists/src/AtividadeTemperatura.java
@@ -5,8 +5,54 @@ mostre todas as temperaturas acima desta média, e em que
 mês elas ocorreram (mostrar o mês por extenso: 1 – Janeiro, 2
 – Fevereiro e etc).*/
 
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class AtividadeTemperatura {
     public static void main(String[] args) {
 
+        List<Double> temperaturas = new ArrayList<Double>();
+
+        Scanner scan = new Scanner(System.in);
+        int contador = 0;
+        while (true){
+            if (contador == 6) break;
+
+            System.out.println("Insira a temperatura: ");
+            Double temperatura = scan.nextDouble();
+            temperaturas.add(temperatura);
+            contador++;
+        }
+
+
+        List<String> meses = new ArrayList<String>();
+        meses.add("Janeiro");
+        meses.add("Fevereiro");
+        meses.add("Março");
+        meses.add("Abril");
+        meses.add("Maio");
+        meses.add("Junho");
+
+
+
+        Double soma = 0d;
+        for (Double temperatura:temperaturas) {
+            soma += temperatura;
+        }
+        Double media = soma/temperaturas.size();
+        System.out.println("A média é: "+ media);
+
+        int mesAtual = 0;
+
+
+        for (Double temperatura:temperaturas){
+            if(temperatura>=media){
+                System.out.println(temperatura);
+                System.out.println(meses.get(mesAtual));
+            }
+            mesAtual++;
+        }
     }
 }
