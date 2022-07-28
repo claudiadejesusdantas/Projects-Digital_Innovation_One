833975a0a5936821cc48756cbe80a492dde8ef2c
Claudia de Jesus Dantas <101577825+claudiadejesusdantas@users.noreply.github.com>
2022-07-28 13:40:47 -0300

    desafios básicos java code girls santander 2022

--- desafios_básicos_Java-CodeGirlsSantander/IndiceDoVetor.java
+++ desafios_básicos_Java-CodeGirlsSantander/IndiceDoVetor.java
@@ -1,2 +1,19 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class IndiceDoVetor {
+    public static void main(String[] args) throws IOException {
+
+        Scanner input = new Scanner(System.in);
+        String[] nomes = new String[];
+        for (int i = 0; i < nomes.length; i++)
+        {
+// TODO: complete os espaços em branco com sua solução para o problema
+            nomes[ ] = input.nextLine();
+        }
+
+        int pos = input.nextInt();
+        System.out.println(            );
+
+    }
 }
