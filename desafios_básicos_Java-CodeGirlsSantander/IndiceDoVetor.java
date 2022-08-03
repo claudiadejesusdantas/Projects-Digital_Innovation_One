c2ff09e58569801e10efa796e9f9e3ef330b15d7
Claudia de Jesus Dantas <101577825+claudiadejesusdantas@users.noreply.github.com>
2022-08-03 18:52:45 -0300

    alteração em um exercicio dio

--- desafios_básicos_Java-CodeGirlsSantander/IndiceDoVetor.java
+++ desafios_básicos_Java-CodeGirlsSantander/IndiceDoVetor.java
@@ -1,19 +1,18 @@
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+
 import java.io.IOException;
 import java.util.Scanner;
 
-public class IndiceDoVetor {
+public class App {
+
     public static void main(String[] args) throws IOException {
 
         Scanner input = new Scanner(System.in);
-        String[] nomes = new String[];
+        String[] nomes = new String[5];
         for (int i = 0; i < nomes.length; i++)
-        {
-// TODO: complete os espaços em branco com sua solução para o problema
-            nomes[ ] = input.nextLine();
-        }
-
-        int pos = input.nextInt();
-        System.out.println(            );
+        { nomes[i] = input.nextLine();}
+        int i = input.nextInt();
+        System.out.println( nomes[i] );
 
     }
 }
